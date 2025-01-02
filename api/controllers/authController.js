1fab72bdc9acea74fa4b08de378bc7b05ada21d4
pedrogomes107 <pedromcgomes95@gmail.com>
2025-01-02 21:37:41 +0000

    Create authControllers Login and Logout

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -60,6 +60,45 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
 
-export const logout = async (req, res) => {};
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = signToken(user._id);
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      httpOnly: true, // prevents XSS attacks
+      sameSite: "strict", // prevents CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+};
